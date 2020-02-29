cd0223423fec2c56aaa904ded0140472f374aed6
srcnkl <sercankaliu@gmail.com>
2020-03-01 01:08:36 +0300

    application created

---
+++ src/main/java/com/sercankal/jetshopping/JetshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.sercankal.jetshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetshoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sercankal/jetshopping/JetshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sercankal.jetshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
